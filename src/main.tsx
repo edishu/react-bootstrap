0c4aa6898cc3b705f359d45b3e6f4f7f2c798cf0
Udayan Maurya <edmauryaishu@gmail.com>
2023-11-19 19:46:45 -0500

    install react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
-import "index.css";
+// Core
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+
+// CSS
+import "index.css";
+
+// Utils
 import { store } from "store/store";
+import { queryClient } from "cache/queryClient";
+
+// Components
+import { Provider } from "react-redux";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { App } from "App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </Provider>
 );
