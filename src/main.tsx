70ff76952204aea9fafa377a6855530ca006abc0
Udayan Maurya <edmauryaishu@gmail.com>
2023-11-19 20:00:17 -0500

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,11 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { App } from "App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <App />
